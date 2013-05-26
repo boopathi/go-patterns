b2a1df18b675dece2667aed882b55d1856928cdc
Boopathi Rajaa <me@boopathi.in>
2013-05-26 19:56:33 +0530

    TypeCasting

--- typecast.go
+++ typecast.go
@@ -9,16 +9,16 @@ type mystring string
 func (m *mystring) modify() {
   *m = mystring("NEW")
 }
-func (m mystring) dont_modify() {
+func (m mystring) doesnt_modify() {
   m = mystring("NEW");
 }
 
 func main() {
   var x mystring
   x = mystring("OLD")
-  fmt.Print(x + "\nCalling dont_modify(): x=")
-  x.dont_modify()
-  fmt.Print(x + "\nCalling modify(): x=")
+  fmt.Println("x = ", x)
+  x.doesnt_modify()
+  fmt.Println("x = ", x, "#doesn't modify x")
   x.modify()
-  fmt.Println(x)
+  fmt.Println("x = ", x, "#modifies x")
 }
